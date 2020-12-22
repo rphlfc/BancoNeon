c0c26b3bc571a9890c7595dbc97ad6198c26eded
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-12-22 19:39:45 -0300

    Initial Commit

---
+++ BancoNeon/BancoNeonApp.swift
@@ -0,0 +1,17 @@
+//
+//  BancoNeonApp.swift
+//  BancoNeon
+//
+//  Created by Raphael Cerqueira on 22/12/20.
+//
+
+import SwiftUI
+
+@main
+struct BancoNeonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
